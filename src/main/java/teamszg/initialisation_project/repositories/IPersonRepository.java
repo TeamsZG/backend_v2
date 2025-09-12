8c1574d0b3c1d5c2ab85a8f1c5fef9346f8241fa
nassimelhaji <nassimelhaji@gmail.com>
2025-09-12 12:02:24 -0400

    Feature2: entité Person créé, PersonService créé, PersonController créé et IPersonRepository créé.

--- src/main/java/teamszg/initialisation_project/repositories/IPersonRepository.java
+++ src/main/java/teamszg/initialisation_project/repositories/IPersonRepository.java
@@ -1,8 +1,10 @@
 package teamszg.initialisation_project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import teamszg.initialisation_project.models.Person;
 
-public interface IPersonRepository extends JpaRepository<Person, Long> {
 
+public interface IPersonRepository extends JpaRepository<Person, Long> {
+    Person findPersonByName(String name);
 }
--- src/main/java/teamszg/initialisation_project/services/PersonService.java
+++ src/main/java/teamszg/initialisation_project/services/PersonService.java
@@ -1,4 +1,23 @@
 package teamszg.initialisation_project.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import teamszg.initialisation_project.models.Person;
+import teamszg.initialisation_project.repositories.IPersonRepository;
+
+import java.util.List;
+@Service
 public class PersonService {
+
+    @Autowired
+    private final IPersonRepository personRepository;
+
+    public PersonService(IPersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+
+    public List<Person> search(String personName){
+        return personRepository.findAll();
+    }
 }
