ce247196b7b10d88a5734c511851c29a865e8ab1
Juba Redjradj <jubaredjradj@gmail.com>
2025-09-13 23:20:09 -0400

    Amélioration gitflow, correction intégralité du code avec amélioration afin de correspondre au maximum des demandes du superviseur

--- src/main/java/teamszg/initialisation_project/repositories/IPersonRepository.java
+++ src/main/java/teamszg/initialisation_project/repositories/IPersonRepository.java
@@ -3,9 +3,13 @@ package teamszg.initialisation_project.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import teamszg.initialisation_project.models.Person;
 
+import java.util.List;
+
 
 public interface IPersonRepository extends JpaRepository<Person, Long> {
-    Person findPersonByName(String name);
+    Person findPersonById(Long id);
+    List<Person> findAllByName(String name);
+
 
 
 }
