140fbe12f4a9f436c00296f8683275a9c64938a7
Juba Redjradj <112187141+FastGlory@users.noreply.github.com>
2025-10-15 11:33:08 -0400

    Revert "Feature/evaluation epic3"

--- src/main/java/teamszg/initialisation_project/repositories/IEpisodeRepository.java
+++
@@ -1,9 +0,0 @@
-package teamszg.initialisation_project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import teamszg.initialisation_project.models.Episode;
-
-public interface IEpisodeRepository extends JpaRepository<Episode, Long> {
-    Episode findEpisodeById(Long episodeId);
-
-}
